877d2f0670758c3ac9a6ea6ad511df0bad864bd8
S.J.R. van Schaik <stephan@synkhronix.com>
2023-03-02 00:40:53 -0500

    macos: implement functions to map/unmap/protect memory in the traced process

--- src/macos/memory.rs
+++ src/macos/memory.rs
@@ -1,10 +1,113 @@
-use crate::{Error, Tracee};
+use crate::{Error, Protection, Tracee, Tracer};
 use mach2::kern_return::KERN_SUCCESS;
-use mach2::vm::{mach_vm_protect, mach_vm_read_overwrite, mach_vm_region, mach_vm_write};
-use mach2::vm_prot::{VM_PROT_COPY, VM_PROT_READ, VM_PROT_WRITE};
+use mach2::vm::{mach_vm_allocate, mach_vm_deallocate, mach_vm_protect, mach_vm_read_overwrite, mach_vm_region, mach_vm_write};
+use mach2::vm_prot::{VM_PROT_COPY, VM_PROT_EXECUTE, VM_PROT_READ, VM_PROT_WRITE};
 use mach2::vm_region::VM_REGION_BASIC_INFO_64;
 use mach2::vm_region::{vm_region_info_t, vm_region_basic_info_64};
 use mach2::vm_types::mach_vm_address_t;
+use std::ops::Range;
+
+impl Tracer {
+    /// Maps memory in the virtual address space of the traced process. Returns the virtual address
+    /// of the new memory range that was allocated.
+    pub fn map_memory(
+        &mut self,
+        tracee: Tracee,
+        size: usize,
+        protection: Protection,
+    ) -> Result<(Tracee, usize), Error> {
+        let mut flags = 0;
+
+        if protection.contains(Protection::READ) {
+            flags |= VM_PROT_READ;
+        }
+
+        if protection.contains(Protection::WRITE) {
+            flags |= VM_PROT_WRITE;
+        }
+
+        if protection.contains(Protection::EXECUTE) {
+            flags |= VM_PROT_EXECUTE;
+        }
+
+        let mut address = 0;
+
+        let result = unsafe {
+            mach_vm_allocate(
+                tracee.task,
+                &mut address,
+                size as _,
+                flags,
+            )
+        };
+
+        if result != KERN_SUCCESS {
+            return Err(Error::Mach(result));
+        }
+
+        Ok((tracee, address as _))
+    }
+
+    /// Unmaps the specified range from the virtual address space of the traced process.
+    pub fn unmap_range(
+        &mut self,
+        tracee: Tracee,
+        range: Range<usize>,
+    ) -> Result<Tracee, Error> {
+        let result = unsafe {
+            mach_vm_deallocate(
+                tracee.task,
+                range.start as _,
+                range.len() as _,
+            )
+        };
+
+        if result != KERN_SUCCESS {
+            return Err(Error::Mach(result));
+        }
+
+        Ok(tracee)
+    }
+
+    /// Changes the protection of the specified range in the virtual address space of the traced
+    /// process.
+    pub fn protect_range(
+        &mut self,
+        tracee: Tracee,
+        range: Range<usize>,
+        protection: Protection,
+    ) -> Result<Tracee, Error> {
+        let mut flags = 0;
+
+        if protection.contains(Protection::READ) {
+            flags |= VM_PROT_READ;
+        }
+
+        if protection.contains(Protection::WRITE) {
+            flags |= VM_PROT_WRITE;
+        }
+
+        if protection.contains(Protection::EXECUTE) {
+            flags |= VM_PROT_EXECUTE;
+        }
+
+        let result = unsafe {
+            mach_vm_protect(
+                tracee.task,
+                range.start as _,
+                range.len() as _,
+                false as _,
+                flags,
+            )
+        };
+
+        if result != KERN_SUCCESS {
+            return Err(Error::Mach(result));
+        }
+
+        Ok(tracee)
+    }
+}
 
 impl Tracee {
     /// Reads the data at the virtual address from the traced process.
